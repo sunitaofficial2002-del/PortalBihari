6ffe14a4c9b4ecbc4a00b2dc05626afb929ad430
sunitaofficial2002-del <sunitaofficial2002@gmail.com>
2026-09-20 19:45:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,21 +7,32 @@ const config = {
     const sseUrl = `${config.API_BASE_URL}/api/events`;
     let evtSource;
 
-    function connectSSE() {
-        evtSource = new EventSource(sseUrl);
-        
-        evtSource.onmessage = function(event) {
-            if (event.data === 'update') {
-                console.log('Real-time update received! Refreshing data...');
-                // Trigger a global custom event that other scripts can listen to
-                window.dispatchEvent(new Event('appDataUpdated'));
+    async function connectSSE() {
+        try {
+            const response = await fetch(sseUrl, {
+                headers: {
+                    'ngrok-skip-browser-warning': 'true',
+                    'Accept': 'text/event-stream'
+                }
+            });
+            
+            const reader = response.body.getReader();
+            const decoder = new TextDecoder();
+            
+            while (true) {
+                const { value, done } = await reader.read();
+                if (done) break;
+                
+                const text = decoder.decode(value);
+                if (text.includes('data: update')) {
+                    console.log('Real-time update received! Refreshing data...');
+                    window.dispatchEvent(new Event('appDataUpdated'));
+                }
             }
-        };
-
-        evtSource.onerror = function() {
-            evtSource.close();
-            setTimeout(connectSSE, 3000); // Reconnect on error
-        };
+        } catch (e) {
+            console.warn('SSE connection lost, reconnecting...');
+        }
+        setTimeout(connectSSE, 3000);
     }
 
     connectSSE();
